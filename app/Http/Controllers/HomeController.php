41a55eebaf91f7e480e62e1505aec3583683b102
Austreberto Gomez <austrebertog@gmail.com>
2019-04-23 17:56:25 -0500

    adding user display role on home

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {        
+        $id = \Auth::user()->id;
+        $role = 'N/A';
+
+        if ($id === 1) {
+            $role = 'student';
+        }else if ($id === 2) {
+            $role = 'teacher';
+        }else if ($id === 3) {
+            $role = 'admin';
+        }
+
+        //return view('pages.index', compact('title'));
+        return view('home')->with('role', $role);
+    }
+}
